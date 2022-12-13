c2c1ec71bfd5d7fe60792fae16f08096fc71c832
hellsangga <2099407284@qq.com>
2022-12-13 17:51:48 +0800

    add bilinear interpolation

--- OpencvImplement/OpencvImplement/src/interpolation.cpp
+++ OpencvImplement/OpencvImplement/src/interpolation.cpp
@@ -4,8 +4,6 @@ namespace luo
 {
 	void nearest_neighbor_interpolation(const cv::Mat& ori, cv::Mat& res, float multiple)
 	{
-		CV_Assert(ori.depth() == CV_8U);
-
 		int ori_width = ori.size().width;
 		int ori_height = ori.size().height;
 
@@ -43,8 +41,6 @@ namespace luo
 
 	void bilinear_interpolation(const cv::Mat& ori, cv::Mat& res, float multiple)
 	{
-		CV_Assert(ori.depth() == CV_8U);
-
 		int ori_width = ori.size().width;
 		int ori_height = ori.size().height;
 
@@ -53,18 +49,94 @@ namespace luo
 
 		if (res_width <= 10.0 || res_height <= 10.0)
 			return;
+
+		res = cv::Mat::zeros(round(res_height), round(res_width), ori.type());
+
+		int rows = res.rows;
+		int cols = res.cols * res.channels();
+		int channels = res.channels();
+
+		uchar* res_p;
+		float x = 0.0, y = 0.0, val = 0.0;
+		int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+		int v11 = 0, v12 = 0, v21 = 0, v22 = 0;
+
+		for (int i = 0; i < rows; i++)
+		{
+			y = (i * ori_height) / res_height;
+			y1 = floor(y);
+			y2 = ceil(y);
+			y1 = y1 < ori_height ? y1 : ori_height - 1;
+			y2 = y2 < ori_height ? y2 : ori_height - 1;
+
+			res_p = res.ptr<uchar>(i);
+			for (int j = 0; j < cols; j += channels)
+			{
+				x = ((j / channels) * ori_width) / res_width;
+				x1 = floor(x);
+				x2 = ceil(x);
+				x1 = x1 < ori_width ? x1 : ori_width - 1;
+				x2 = x2 < ori_width ? x2 : ori_width - 1;
+
+				for (int c = 0; c < channels; c++)
+				{
+					if (y1 == y2 && x1 != x2)
+					{
+						v11 = ori.ptr<uchar>(y1)[x1 * channels + c];
+						v12 = ori.ptr<uchar>(y1)[x2 * channels + c];
+						val = v11 + ((v12 - v11) / (x2 - x1)) * (x - x1);
+					}
+					else if (x1 == x2 && y1 != y2)
+					{
+						v11 = ori.ptr<uchar>(y1)[x1 * channels + c];
+						v21 = ori.ptr<uchar>(y2)[x1 * channels + c];
+						val = v11 + ((v21 - v11) / (y2 - y1)) * (y - y1);
+					}
+					else if (x1 == x2 && y1 == y2)
+					{
+						val = ori.ptr<uchar>(y1)[x1 * channels + c];
+					}
+					else
+					{
+						v11 = ori.ptr<uchar>(y1)[x1 * channels + c];
+						v12 = ori.ptr<uchar>(y1)[x2 * channels + c];
+						v21 = ori.ptr<uchar>(y2)[x1 * channels + c];
+						v22 = ori.ptr<uchar>(y2)[x2 * channels + c];
+
+						float q1 = v11 + ((v12 - v11) / (x2 - x1)) * (x - x1);
+						float q2 = v21 + ((v22 - v21) / (x2 - x1)) * (x - x1);
+
+						val = q1 + ((q2 - q1) / (y2 - y1)) * (y - y1);
+					}
+
+					res_p[j + c] = round(val);
+				}
+			}
+		}
 	}
-}
 
-void test_interpolation()
-{
-	cv::Mat mat = cv::imread("D:/testpic/cv2.PNG");
-	if (mat.empty())
-		return;
+	enum interpolation_method
+	{
+		NEAREST_NEIGHBOR_INTERPOLATION,
+		BILINEAR_INTERPOLATION
+	};
 
-	cv::imshow("mat", mat);
+	void interpolation(const cv::Mat& ori, cv::Mat& res, float multiple, int method)
+	{
+		CV_Assert(ori.depth() == CV_8U);
 
-	cv::Mat res;
-	luo::nearest_neighbor_interpolation(mat, res, 0.6);
-	cv::imshow("res", res);
+		switch (method)
+		{
+		case NEAREST_NEIGHBOR_INTERPOLATION:
+			nearest_neighbor_interpolation(ori, res, multiple);
+			break;
+			
+		case BILINEAR_INTERPOLATION:
+			bilinear_interpolation(ori, res, multiple);
+			break;
+
+		default:
+			break;
+		}
+	}
 }
--- OpencvImplement/OpencvImplement/src/main.cpp
+++ OpencvImplement/OpencvImplement/src/main.cpp
@@ -1,10 +1,31 @@
 #include "pch.h"
 
-void test_interpolation();
+namespace luo
+{
+	enum interpolation_method
+	{
+		NEAREST_NEIGHBOR_INTERPOLATION,
+		BILINEAR_INTERPOLATION
+	};
+
+	void interpolation(const cv::Mat& ori, cv::Mat& res, float multiple, int method);
+}
 
 int main()
 {
-	test_interpolation();
+	cv::Mat mat = cv::imread("D:/testpic/cv2.PNG");
+	if (mat.empty())
+		return -1;
+
+	cv::imshow("mat", mat);
+
+	cv::Mat res;
+	luo::interpolation(mat, res, 1.5, luo::NEAREST_NEIGHBOR_INTERPOLATION);
+	cv::imshow("res", res);
+
+	cv::Mat res2;
+	luo::interpolation(mat, res2, 1.5, luo::BILINEAR_INTERPOLATION);
+	cv::imshow("res2", res2);
 
 	cv::waitKey(0);
 }
